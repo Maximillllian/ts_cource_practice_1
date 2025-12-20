f4a3b854a12293fe331e279c0c325d5f36d67db3
Maxim Woronin <voronin.maksim@mail.com>
2025-12-20 18:20:13 +0500

    initial typing of patch

--- src/patch.ts
+++ src/patch.ts
@@ -1,59 +1,109 @@
-export function applyOpsInPlace(obj, ops) {
+import assert from "node:assert";
+
+type Operation = {
+    path: string, // ???
+    op: "set" | "unset" | "push" | "inc",
+    value: unknown, // ???
+}
+
+type UnknownObject = Record<string, unknown>;
+
+function assertNever(_operation: never) {
+    throw new Error("Unexpected situation");
+}
+
+export function applyOpsInPlace(obj: UnknownObject, ops: Operation[]) {
   for (const op of ops || []) {
-    const path = String(op.path || "").trim();
+    const path = op.path || "".trim();
     if (!path) continue;
 
-    if (op.op === "set") setByPath(obj, path, op.value);
-    if (op.op === "unset") unsetByPath(obj, path);
-    if (op.op === "push") pushByPath(obj, path, op.value);
-    if (op.op === "inc") incByPath(obj, path, op.value);
+    if (op.op === "set") {
+        setByPath(obj, path, op.value);
+        return;
+    };
+
+    if (op.op === "unset") {
+        unsetByPath(obj, path);
+        return;
+    }
+
+    if (op.op === "push") {
+        pushByPath(obj, path, op.value);
+        return;
+    }
+
+    if (op.op === "inc") {
+        incByPath(obj, path, op.value);
+        return;
+    }
+
+    assertNever(op.op);
   }
 }
 
-function setByPath(obj, path, value) {
-  const parts = path.split(".");
+
+function setByPath(obj: UnknownObject, path: string, value: unknown) {
+  const parts = path.split(".");  
   let cur = obj;
   for (let i = 0; i < parts.length - 1; i++) {
-    const k = parts[i];
-    cur[k] ??= {};
-    cur = cur[k];
+      const k = parts[i];
+      assert.ok(k);
+
+      cur[k] ??= {}; 
+
+      const nextPart = cur[k];
+      assert(nextPart && typeof nextPart === 'object');
+      cur = cur[k] as UnknownObject;
   }
-  cur[parts.at(-1)] = value;
+    
+  const lastPart = parts.at(-1);
+  assert.ok(lastPart);
+
+  cur[lastPart] = value;
 }
 
-function unsetByPath(obj, path) {
+function unsetByPath(obj: UnknownObject, path: string) {
   const parts = path.split(".");
   let cur = obj;
   for (let i = 0; i < parts.length - 1; i++) {
     const k = parts[i];
     if (cur == null) return;
-    cur = cur[k];
-  }
-  if (cur) delete cur[parts.at(-1)];
+    assert(k);
+    cur = cur[k] as UnknownObject;
+    }
+    
+    const lastPart = parts.at(-1);
+    assert(lastPart);
+  if (cur) delete cur[lastPart];
 }
 
-function pushByPath(obj, path, value) {
+function pushByPath(obj: UnknownObject, path: string, value: unknown) {
   const parts = path.split(".");
   let cur = obj;
   for (let i = 0; i < parts.length - 1; i++) {
     const k = parts[i];
+    assert(k);
     cur[k] ??= {};
-    cur = cur[k];
+    cur = cur[k] as UnknownObject;
   }
   const last = parts.at(-1);
+  assert(last);
   if (!Array.isArray(cur[last])) cur[last] = [];
+  assert(Array.isArray(cur[last]));  
   cur[last].push(value);
 }
 
-function incByPath(obj, path, value) {
+function incByPath(obj: UnknownObject, path: string, value: unknown) {
   const parts = path.split(".");
   let cur = obj;
   for (let i = 0; i < parts.length - 1; i++) {
     const k = parts[i];
+    assert(k);  
     cur[k] ??= {};
-    cur = cur[k];
+    cur = cur[k] as UnknownObject;
   }
   const last = parts.at(-1);
+  assert(last);
   cur[last] =
     (typeof cur[last] === "number" ? cur[last] : 0) + (Number(value) || 0);
 }
